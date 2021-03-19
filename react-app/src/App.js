db22a27deb561e2d8d6924db88bcfd763d341325
cahe6160 <cahe6160@colorado.edu>
2021-03-19 12:12:24 -0600

    removed map api

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,30 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ComboBox from './components/ComboBox.js';
-import MapChart from './components/MapChart.js';
 import React from 'react';
-//import MapChart from './components/MapChart';
-import Map from './components/map/Map.js';
 
-/*
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Header2
         <img src={logo} className="App-logo" alt="logo" />
         <ComboBox/>
-        <MapChart/>
-      </header>
-      <MapSection location={location} zoomLevel={17} /> 
     </div>
   );
 }
-*/
-function App() {
-    return (
-      <Map/>
-    );
-  }
+
 
 export default App;
