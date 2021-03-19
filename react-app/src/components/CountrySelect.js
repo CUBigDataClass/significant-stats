bbe16a2aef0960b93ee318d9160c2991be976ca5
cahe6160 <cahe6160@colorado.edu>
2021-03-19 12:31:57 -0600

    minor country box change

--- react-app/src/components/CountrySelect.js
+++ react-app/src/components/CountrySelect.js
@@ -30,7 +30,7 @@ export default function CountrySelect() {
   return (
     <Autocomplete
       id="country-select-demo"
-      style={{ width: 300 }}
+      style={{ width: 500 }}
       options={countries}
       classes={{
         option: classes.option,
