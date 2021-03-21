688871513131c43231145a229e77c5b33097541e
dyqu1748 <dyqu1748@colorado.edu>
2021-03-20 18:23:24 -0600

    App page now able to take user input and display custom query data

--- react-app/flask-api/app.py
+++ react-app/flask-api/app.py
@@ -1,7 +1,6 @@
 import time
 from flask import Flask
 from cassandra.cluster import Cluster
-import json
 
 
 cluster = Cluster(['34.71.38.163','34.69.92.195','35.225.100.34'])
@@ -13,9 +12,30 @@ app = Flask(__name__)
 def get_current_time():
     return {'time': time.time()}
 
-@app.route('/data')
-def get_temp_state():
-     quer = "SELECT * FROM us_state_temp WHERE state='Colorado' LIMIT 2 ALLOW FILTERING;"
+@app.route('/state_temp/<state>/<startYear>/<endYear>')
+def get_state_temp(state,startYear,endYear):
+     quer = "SELECT * FROM us_state_temp WHERE state='"+state+"' AND year>="+startYear+" AND year<="+endYear+" ALLOW FILTERING;"
+     data = session.execute(quer)
+    
+     desc = data.column_names
+     column_names = [col for col in desc]
+#     for row in data:
+#         avg = row.average_temp
+     res= [dict(zip(column_names, row))  
+             for row in data]
+#     json_object = json.dumps(res)   
+     return {'result':res}
+#    quer = "SELECT * FROM us_state_temp WHERE state='"+cond[0]+"'"
+#    if (len(cond) == 1):
+#        quer += " ALLOW FILTERING;"
+#    elif (len(cond) == 2):
+#        quer += " AND year="+str(cond[1])+" ALLOW FILTERING;"
+#    else:
+#        quer += " AND year>="+str(cond[1])+" AND year <="+str(cond[2])+" ALLOW FILTERING;"
+     
+@app.route('/country_temp/<country>/<startYear>/<endYear>')
+def get_country_temp(country,startYear,endYear):
+     quer = "SELECT * FROM global_land_temp WHERE country='"+country+"' AND year>="+startYear+" AND year<="+endYear+" ALLOW FILTERING;"
      data = session.execute(quer)
     
      desc = data.column_names
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,14 +6,88 @@ import ComboBox from './components/ComboBox.js';
 import MapChart from './components/MapChart.js';
 //import MapChart from './components/MapChart';
 
-const getData = async() =>
-  await fetch(`/data`)
-  .then(res => (res.ok ? res : Promise.reject(res)))
-  .then(res => res.json())
+// const getData = async() =>
+//   await fetch(`/data/Colorado`)
+//   .then(res => (res.ok ? res : Promise.reject(res)))
+//   .then(res => res.json())
 
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [stateName, setStateName] = useState('');
+  const [country, setCountry] = useState('');
+  const [startYear, setStartYear] = useState('');
+  const [endYear, setEndYear] = useState('');
+  const [stateTempData, setStateTempData] = useState([]);
+  const [countryTempData, setCountryTempData] = useState([]);
+  const [errorMessage,setErrorMessage] = useState('');
+ 
+  const handleSubmitState = (evt) => {
+    evt.preventDefault();
+    setCountryTempData([]);
+    fetchDataState();
+  }
+
+  const handleSubmitCountry = (evt) => {
+    evt.preventDefault();
+    setStateTempData([]);
+    fetchDataCountry();
+  }
+
+  async function fetchDataState(){
+    const url = '/state_temp/'+stateName+'/'+startYear+'/'+endYear;
+    try{
+      const response = await fetch(url);
+      const json = await response.json();
+      if (!response.ok){
+        const error = (json && json.message) || response.statusText;
+        setErrorMessage(error);
+        return Promise.reject(error);
+      }
+      setStateTempData(json.result);
+
+    } catch (error){
+      setErrorMessage(error.toString());
+      console.log(error);
+    }
+  }
+
+  async function fetchDataCountry(){
+    const url = '/country_temp/'+country+'/'+startYear+'/'+endYear;
+    try{
+      const response = await fetch(url);
+      const json = await response.json();
+      if (!response.ok){
+        const error = (json && json.message) || response.statusText;
+        setErrorMessage(error);
+        return Promise.reject(error);
+      }
+      setCountryTempData(json.result);
+
+    } catch (error){
+      setErrorMessage(error.toString());
+      console.log(error);
+    }
+  }
+
+  // useEffect(async () => {
+  //   fetch("/data/"+stateName+'/'+year)
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setTempData(result.result);
+  //       },
+  //       // Note: it's important to handle errors here
+  //       // instead of a catch() block so that we don't swallow
+  //       // exceptions from actual bugs in components.
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     )
+  // }, [])
+  
 
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
@@ -28,7 +102,7 @@ function App() {
         <ComboBox/>
         <MapChart/>
         <p>The current time is {currentTime}.</p>
-        <Async promiseFn={getData}>
+        {/* <Async promiseFn={getData}>
           <Async.Loading>Loading...</Async.Loading>
           <Async.Fulfilled>
             {data => {
@@ -53,7 +127,74 @@ function App() {
           <Async.Rejected>
             {error => `Something went wrong: ${error.message}`}
           </Async.Rejected>
-      </Async>
+      </Async> */}
+      <form onSubmit={handleSubmitState}>
+      <label>
+        State:
+        <input
+          type="text"
+          value={stateName}
+          onChange={event => setStateName(event.target.value)}
+        />
+      </label>
+      <label>
+        Start Year:
+        <input
+          type="number"
+          value={startYear}
+          onChange={event => setStartYear(event.target.value)}
+        />
+      </label>
+      <label>
+        End Year:
+        <input
+          type="number"
+          value={endYear}
+          onChange={event => setEndYear(event.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    <form onSubmit={handleSubmitCountry}>
+      <label>
+        Country:
+        <input
+          type="text"
+          value={country}
+          onChange={event => setCountry(event.target.value)}
+        />
+      </label>
+      <label>
+        Start Year:
+        <input
+          type="number"
+          value={startYear}
+          onChange={event => setStartYear(event.target.value)}
+        />
+      </label>
+      <label>
+        End Year:
+        <input
+          type="number"
+          value={endYear}
+          onChange={event => setEndYear(event.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    <ul>
+          {stateTempData.map(row => (
+            <li>
+              {row.state}, Average Temp: {row.average_temp}, Year: {row.year}
+            </li>
+          ))}
+          {countryTempData.map(row => (
+            <li>
+              {row.country}, Average Temp: {row.average_temp}, Year: {row.year}
+            </li>
+          ))}
+        </ul>
+        <p>{errorMessage}</p>
       </header>
     </div>
   );
