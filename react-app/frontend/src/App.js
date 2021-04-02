b14649cc38e45b3af1f8660977d571825f877f0c
Casey Tran <caseyqtran@gmail.com>
2021-04-02 11:48:27 -0600

    Fixed App.js

--- react-app/frontend/src/App.js
+++ react-app/frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('https://significant-stats.herokuapp.com//flask/hello').then(response => {
+    axios.get('https://significant-stats.herokuapp.com/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
