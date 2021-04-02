f3dce0cdab9820a04c620d4b0c636f2f80e408d0
casey-tran <43631898+casey-tran@users.noreply.github.com>
2021-04-02 12:55:59 -0600

    Cleaned up app.py from unnecessary files

--- react-app/flask-api/app.py
+++ react-app/flask-api/app.py
@@ -1,7 +1,5 @@
-import time
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from HelloApiHandler import HelloApiHandler
 from cassandra.cluster import Cluster
 
 
@@ -9,17 +7,10 @@ cluster = Cluster(['34.71.38.163','34.69.92.195','35.225.100.34'])
 session = cluster.connect('firstkeyspace')
 
 app = Flask(__name__, static_url_path='', static_folder='../frontend/build')
-api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
-
-api.add_resource(HelloApiHandler, '/flask/hello')
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 
 @app.route('/state_temp/<state>/<startYear>/<endYear>')
 def get_state_temp(state,startYear,endYear):
